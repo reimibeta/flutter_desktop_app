4d69b03783a97e5145e9319bbc526729801088ff
reimibeta <65539218+reimibeta@users.noreply.github.com>
2020-09-09 22:35:06 +0700

    add chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:vector_math/vector_math.dart' hide Colors;
 
-void main() {
-  runApp(MyApp());
-}
+import 'bar_chart/bar_chart_page.dart';
+import 'bar_chart/bar_chart_page2.dart';
+// import 'line_chart/line_chart_page.dart';
+// import 'line_chart/line_chart_page2.dart';
+// import 'line_chart/line_chart_page3.dart';
+// import 'line_chart/line_chart_page4.dart';
+// import 'pie_chart/pie_chart_page.dart';
+// import 'scatter_chart/scatter_chart_page.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FlChart Demo',
+      showPerformanceOverlay: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: const Color(0xff262545),
+        primaryColorDark: const Color(0xff201f39),
+        brightness: Brightness.dark,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'fl_chart'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
@@ -51,84 +36,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-        actions: [
-            IconButton(icon: Icon(Icons.speaker),onPressed: () {
-              // Define a ray starting at the origin and going into positive x-direction.
-              Ray ray = new Ray.originDirection(new Vector3.zero(), new Vector3(1.0, 0.0, 0.0));
-              // Defines a sphere with the center (5.0 0.0 0.0) and a radius of 2.
-              Sphere sphere = new Sphere.centerRadius(new Vector3(5.0, 0.0, 0.0), 2.0);
-              // Checks if the ray intersect with the sphere and returns the distance of the
-              // intersection from the origin of the ray. Would return null if no intersection
-              // is found.
-              double distanceFromOrigin = ray.intersectsWithSphere(sphere);
-              // Evaluate the position of the intersection, in this case (3.0 0.0 0.0).
-              Vector3 position = ray.at(distanceFromOrigin);
-              print("Position: $position");
-            },
-          )
-        ],
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: SafeArea(
+        child: Container(
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  // height: 100,
+                  child: BarChartPage(),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  // height: 100,
+                  child: BarChartPage2(),
+                ),
+              )
+            ],
+          ),
         ),
+        // child: PageView(
+        //   children: <Widget>[
+        //     // LineChartPage(),
+        //     BarChartPage(),
+        //     BarChartPage2(),
+        //     // PieChartPage(),
+        //     // LineChartPage2(),
+        //     // LineChartPage3(),
+        //     // LineChartPage4(),
+        //     // ScatterChartPage(),
+        //   ],
+        // ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
